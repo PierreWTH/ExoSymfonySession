bbc719eba2ed0f88c3314595e84db5b7df6f4b4a
PierreWTH <wietrichpierre@gmail.com>
2023-04-14 16:21:04 +0200

    add/edit/remove session ok

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Formation;
+use App\Form\FormationType;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,5 +20,56 @@ class FormationController extends AbstractController
             'formations' => $formations,
             
         ]);
+    }
+
+     // Ajouter ou editer un stagiaire
+     #[Route('/formation/add', name: 'add_formation')]
+     #[Route('/formation/{id}/edit', name: 'edit_formation')]
+ 
+     public function add(ManagerRegistry $doctrine, Formation $formation = null, Request $request) : response
+     {
+ 
+         if(!$formation){
+             $formation = new formation();
+         }
+ 
+         // Creation du formulaire et objet qu'on lui fait passer
+         $form = $this->createForm(FormationType::class, $formation);
+ 
+         //Récuperation des données du formulaire
+         $form->handleRequest($request);
+ 
+         // Vérification si le form est soumis et si les données sont saines
+         if($form->isSubmitted() && $form->isValid())
+         {   
+             // On hydrate l'objet formation qu'on a crée avec les données du formulaire
+             $formation = $form->getData();
+             // On récupère l'entity Manager pour avoir acces a persist et flush
+             $entityManager = $doctrine->getManager();
+             // On prépare l'objet
+             $entityManager->persist($formation);
+             // On l'execute
+             $entityManager->flush();
+ 
+             return $this->redirectToRoute('app_formation');
+         }
+ 
+         // Vue qui vas afficher le formulaire
+         return $this->render('formation/add.html.twig', [
+             //generer le formulaire visuellement quand on utilise formAddformation
+             'formAddFormation' => $form->createView()
+             
+         ]);
+     }
+
+     // Supprimer une formation
+    #[Route('/formation/{id}/delete', name: 'delete_formation')]
+    public function delete(ManagerRegistry $doctrine, Formation $formation): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($formation);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_formation');
     }
 }

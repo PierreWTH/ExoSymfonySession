025165ceb578452a8ae493f1ce39b6ec7b88018c
PierreWTH <wietrichpierre@gmail.com>
2023-04-14 16:12:16 +0200

    add/edit/remove categorie

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Categorie;
+use App\Form\CategorieType;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,6 +19,56 @@ class CategorieController extends AbstractController
         return $this->render('categorie/index.html.twig', [
             'categories' => $categories,
         ]);
+    }
+
+     // Ajouter ou editer un stagiaire
+     #[Route('/categorie/add', name: 'add_categorie')]
+     #[Route('/categorie/{id}/edit', name: 'edit_categorie')]
+    public function add(ManagerRegistry $doctrine, Categorie $categorie = null, Request $request) : response
+    {
+
+        if(!$categorie){
+            $categorie = new Categorie();
+        }
+
+        // Creation du formulaire et objet qu'on lui fait passer
+        $form = $this->createForm(CategorieType::class, $categorie);
+
+        //Récuperation des données du formulaire
+        $form->handleRequest($request);
+
+        // Vérification si le form est soumis et si les données sont saines
+        if($form->isSubmitted() && $form->isValid())
+        {   
+            // On hydrate l'objet Categorie qu'on a crée avec les données du formulaire
+            $categorie = $form->getData();
+            // On récupère l'entity Manager pour avoir acces a persist et flush
+            $entityManager = $doctrine->getManager();
+            // On prépare l'objet
+            $entityManager->persist($categorie);
+            // On l'execute
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_categorie');
+        }
+
+        // Vue qui vas afficher le formulaire
+        return $this->render('categorie/add.html.twig', [
+            //generer le formulaire visuellement quand on utilise formAddcategorie
+            'formAddCategorie' => $form->createView()
+            
+        ]);
+    }
+
+    // Supprimer une categorie
+    #[Route('/categorie/{id}/delete', name: 'delete_categorie')]
+    public function delete(ManagerRegistry $doctrine, Categorie $categorie): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($categorie);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_categorie');
     }
 
 }
